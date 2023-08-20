024ea18746cdaa9bbb6c5126e15145cf00b2071f
salmanbinmoosa <137569545+salmanbinmoosa@users.noreply.github.com>
2023-08-20 15:36:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,20 +13,53 @@ xmark.addEventListener("click" , function() {
 const circle = document.getElementsByClassName("circle")[0]
 const pause = document.getElementsByClassName("pause")[0]
 const play = document.getElementsByClassName("play")[0]
+const pausetwo = document.getElementsByClassName("pause2")[0]
+const playtwo = document.getElementsByClassName("play2")[0]
 
 // song //
 const Alone = new Audio ("Alan Walker _ Ava Max - Alone_ Pt. II(MP3_160K).mp3")
 
-
+const playContainer = document.getElementsByClassName("play-container")[0]
 
 circle.addEventListener("click" , function() {
     if( pause.style.display !== "none" ){
         pause.style.display = "none"
+        pausetwo.style.display = "none"
         play.style.display = "block"
+        playtwo.style.display = "block"
+        circle.style.display = "flex"
+        playContainer.style.display = "flex"
+        footer.style.display = "none"
         Alone.play()   
     } else{
         pause.style.display = "block"
         play.style.display = "none"   
+        playtwo.style.display = "none"   
+        pausetwo.style.display = "block"
+
         Alone.pause()
     }
+})
+
+
+
+// responsive designs // 
+
+var viewportWidth = window.innerWidth;
+
+var breakpoint = 768;
+
+
+
+// //
+
+const home = document.getElementById("home")
+const search = document.getElementById("search")
+
+const right = document.getElementsByClassName("right")[0]
+const left = document.getElementsByClassName("left")[0]
+
+home.addEventListener("click", function(){
+    right.style.display = "block"
+    left.style.display = "none"
 })
